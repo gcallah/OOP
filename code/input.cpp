617ab94971410844b3158d9ee6cee39aeaf11050
gcallah <gcallah@mac.com>
2018-06-23 02:53:50 -0400

    Cleaning up chapter 3.

---
+++ code/input.cpp
@@ -0,0 +1,10 @@
+#include "std_lib_facilities.h"
+
+int main() {
+    cout << "Please enter your name:\n";
+    string first_name; // variable of type string
+    int age;
+    cin > > first_name; // read characters into first name
+    cin >> age;
+    cout << first_name << age;
+} 
