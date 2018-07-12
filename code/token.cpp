24a6c43c4c479821c7c60c04da14f199603e20c0
gcallah <gcallah@mac.com>
2018-07-12 15:47:41 -0400

    Added token program.

--- code/token.cpp
+++ code/token.cpp
@@ -12,33 +12,42 @@ vector<Token> tokens;
 
 int main()
 {
-    Token t = get_token();
-	while(t.kind != 'q') {
+    // frazzyswizel!
+    for(Token t = get_token(); t.kind != 'q'; t = get_token()) {
     	tokens.push_back(t);
-		t = get_token();
 	}
 
 	for(Token tok : tokens) {
-		cout << "Token kind = " << tok.kind << "; val = " << tok.value << '\n';
+        if(tok.kind == '8')
+		    cout << "A number token with val = " << tok.value << '\n';
+        else if(tok.kind == 'i')
+		    cout << "We received an invalid token of value " 
+                << char(tok.value) << '\n';
+        else
+		    cout << "A token of kind " << tok.kind << '\n';
 	}
+    return 0;
 }
 
 Token get_token() {
 	char ch;
 	cin >> ch;
 	switch(ch) {
-	case ';':
-	case 'q':
-	case '(': case ')': case '+': case '-': case '/':
-		return Token{ch};
-	case '.':
-	case '0': case '1': case '2': case '3': case '4':
-	case '5': case '6': case '7': case '8': case '9':
-	{
-		cin.putback(ch);
-		double val;
-		cin >> val;
-		return Token{'8', val};
+    	case ';':
+    	case 'q':
+        case '(': case ')': case '+': case '-': case '/': case '*': case '%':
+    		return Token{ch};
+    	case '.':
+    	case '0': case '1': case '2': case '3': case '4':
+    	case '5': case '6': case '7': case '8': case '9':
+    	{
+    		cin.putback(ch);
+    		double val;
+    		cin >> val;
+    		return Token{'8', val};
+    	}
+        default:
+            return Token{'i', double(ch)};
 	}
-	}
+    return Token{'q'};
 }
