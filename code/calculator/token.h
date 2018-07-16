121780936dce6eadcc5bed6807e618c3be4747ae
gcallah <gcallah@mac.com>
2018-07-16 13:53:11 -0400

    Adding error recovery to calculator.

--- code/calculator/token.h
+++ code/calculator/token.h
@@ -14,6 +14,7 @@ class Token_stream {
     public:
         Token get();
         void putback(Token t);
+        void ignore(char c);
     private:
         bool full{false};
         Token buffer;
